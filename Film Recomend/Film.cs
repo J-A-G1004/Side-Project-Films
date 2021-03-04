0644c46f4bdb337f6b9e9a4838cc19be448b4143
Joe Gillen <j.gillen-2018@hull.ac.uk>
2021-03-04 03:10:50 +0000

    Started project and created a Film Class added member variables and empty constructor for now

---
+++ Film Recomend/Film.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Film_Recomend
+{
+    class Film
+    {
+        public string FilmName { get; private set; }
+        public int ID { get; private set; }
+        public string Description { get; private set; }
+        public string Rating;
+
+
+        public Film () //add consrtuctors later as the addfilm add id add descryption add rating methods will need to be added first 
+        {
+
+        }
+    }
+}
---
+++ Film Recomend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Film_Recomend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
